11efe661762588de6a428900cbfc1969daf9b668
Julien H <>
2023-05-23 23:08:40 +0200

    Add samples for resonstruction and bestPrice queries

--- README.md
+++ README.md
@@ -13,5 +13,52 @@ Install the dependencies specified by the `requirements.txt`
 ## Run
 
 ```shell
-python3 main.py
+$ python3 main.py
+
+sampleStationSearch
+('Frankfurt(Main)Hbf', '8000105')
+('FRANKFURT(MAIN)', '8096021')
+('Frankfurt(M)Flughafen', '8000281')
+('Frankfurt(M) Flughafen Fernbf', '8070003')
+('Frankenthal Hbf', '8000332')
+('Frankfurt(Main)Süd', '8002041')
+('Frankfurt(Oder)', '8010113')
+('Frankfurt(Main)West', '8002042')
+('Frankfurt-Niederrad', '8002050')
+('Frankfurt-Höchst', '8000106')
+('Frankfurt(M) Flughafen Regionalbf', '8070004')
+('Frankfurt(M)Konstablerwache', '8004429')
+('Frankenthal Süd', '8002025')
+('Frankfurt(Main)Ost', '8002039')
+('Frankfurt(M)Mühlberg', '8002034')
+('Frankfurt-Mainkur', '8002048')
+('Frankfurt(M)Galluswarte', '8006690')
+('Frankfurt(M)Lokalbahnhof', '8002038')
+('Frankenberg(Eder)', '8000104')
+('Frankenthal-Flomersh', '8002014')
+('Frankfurt-Sossenheim', '8002054')
+('Frankfurt am Main - Stadion', '8002040')
+('Frankfurt(M)Ostendstraße', '8002058')
+('Frankfurt-Griesheim', '8002046')
+('Frankfurt-Höchst Farbwerke', '8002051')
+
+sampleConnectionSearch
+('C-0', '011700')
+('C-1', '014200')
+('C-2', '013200')
+
+sampleResonstruction
+('Super Sparpreis', 7990)
+('Sparpreis', 8790)
+('Flexpreis', 8820)
+('Flexpreis Plus', 10330)
+('Super Sparpreis', 12390)
+
+sampleBestPriceSearch
+('070000', 1790)
+('100000', 3190)
+('130000', 3590)
+('160000', 3590)
+('190000', 3590)
+('01000000', 2190)
 ```
--- main.py
+++ main.py
@@ -4,15 +4,15 @@ This module provides examples on how to query the DB (Deutsche Bahn) api
 
 import requests
 import json
+import urllib3
+
+# - Hide warnings for insecure connections (proxy ;) )
+#   TODO: Never use this in production
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def searchConnection(enableProxy=False):
-    '''
-    Search for connections between Köln and Frankfurt
-
-    :param enableProxy:
-    :return:
-    '''
+    """ Search for connections between Cologne and Frankfurt """
 
     if enableProxy:
         proxies = {'https': '0.0.0.0:8080'}
@@ -77,10 +77,10 @@ def searchConnection(enableProxy=False):
     return response.json()
 
 
-def searchStationByName(searchTerm, enableProxy=False):
+def searchStationByName(enableProxy=False):
 
-    if searchTerm != 'Frank':
-        raise Exception("Checksum is only valid for searchterm: 'Frank'")
+    # - fix search term, until checksum is calculated dynamically
+    searchTerm = 'Frank'
 
     if enableProxy:
         proxies = {'https': '0.0.0.0:8080'}
@@ -131,33 +131,197 @@ def searchStationByName(searchTerm, enableProxy=False):
     return response.json()
 
 
+def reconstruction(enableProxy=False):
+
+    if enableProxy:
+        proxies = {'https': '0.0.0.0:8080'}
+        verify = False
+    else:
+        proxies = {}
+        verify = True
+
+    url = "https://reiseauskunft.bahn.de/bin/mgate.exe"
+
+    params = {
+        'checksum': 'bac6e939533c25bb9d19781735fb165d',
+    }
+
+    headers = {
+        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 9; unknown Build/PI)",
+        "Content-Type": "application/json;charset=UTF-8"
+    }
+
+    reconstructionRequest = {
+        "auth": {"aid": "n91dB8Z77MLdoR0K", "type": "AID"},
+        "client": {"id": "DB", "name": "DB Navigator", "os": "Android 9", "res": "1080x2028", "type": "AND", "ua": "Dalvik/2.1.0 (Linux; U; Android 9; unknown Build/PI)", "v": 22080000},
+        "ext": "DB.R22.04.a",
+        "formatted": False,
+        "lang": "eng",
+        "svcReqL": [{
+            "cfg": {"polyEnc": "GPA"},
+            "meth": "Reconstruction",
+            "req": {
+                "getPasslist": True,
+                "getPolyline": True,
+                "outReconL": [{
+                    "ctx": "T$A=1@O=Frankfurt(Main)Hbf@L=8000105@a=128@$A=1@O=Köln Hbf@L=8000207@a=128@$202305251828$202305251931$ICE   10$$1$$$$$$"
+                }],
+                "trfReq": {
+                    "cType": "PK",
+                    "directESuiteCall": True,
+                    "jnyCl": 2,
+                    "rType": "DB-PE",
+                    "tvlrProf": [{"type": "E"}]
+                }
+            }
+        }],
+        "ver": "1.15"
+    }
+
+    # - Server fails in case of unicode escape sequences
+    #   hence, we have to handle the json serialization process
+    #   in order to set `ensure_ascii`
+    reconstructionRequestStr = json.dumps(reconstructionRequest, ensure_ascii=False, separators=(',', ':'))
+    reconstructionRequestEncoded = reconstructionRequestStr.encode('utf-8')
+
+    response = requests.post(url, params=params, headers=headers, data=reconstructionRequestEncoded, proxies=proxies, verify=verify)
+    return response.json()
+
+
+def bestPriceSearch(enableProxy=False):
+
+    if enableProxy:
+        proxies = {'https': '0.0.0.0:8080'}
+        verify = False
+    else:
+        proxies = {}
+        verify = True
+
+    url = "https://reiseauskunft.bahn.de/bin/mgate.exe"
+
+    params = {
+        'checksum': 'b7fbc668cec93b4a6d3a24a12d9339cc',
+    }
+
+    headers = {
+        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 9; Pixel 3 Build/PI)",
+        "Content-Type": "application/json;charset=UTF-8"
+        }
+
+    bestPriceSearchRequest = {
+        "auth": {"aid": "n91dB8Z77MLdoR0K", "type": "AID"},
+        "client": {"id": "DB", "name": "DB Navigator", "os": "Android 9", "res": "1080x2028", "type": "AND", "ua": "Dalvik/2.1.0 (Linux; U; Android 9; Pixel 3 Build/PI)", "v": 22080000},
+        "ext": "DB.R22.04.a",
+        "formatted": False,
+        "lang": "eng",
+        "svcReqL": [{
+            "cfg": {"polyEnc": "GPA", "rtMode": "HYBRID"},
+            "meth": "BestPriceSearch",
+            "req": {
+                "outDate": "20230525",
+                "outTime": "183000",
+                "arrLocL": [
+                    {
+                        "crd": {"x": 6959197, "y": 50942823},
+                        "extId": "8000207",
+                        "lid": "A=1@O=Köln Hbf@X=6958730@Y=50943029@U=80@L=8000207@B=1@p=1678909069@",
+                        "name": "Köln Hbf", "type": "S"
+                    }],
+                "depLocL": [{
+                    "crd": {"x": 8663003, "y": 50106817},
+                    "extId": "8000105",
+                    "lid": "A=1@O=Frankfurt(Main)Hbf@X=8663785@Y=50107149@U=80@L=8000105@B=1@p=1678909069@",
+                    "name": "Frankfurt(Main)Hbf",
+                    "type": "S"
+                }],
+                "getPasslist": True,
+                "getPolyline": True,
+                "jnyFltrL": [{
+                    "mode": "BIT",
+                    "type": "PROD",
+                    "value": "11111111111111"
+                }],
+                "trfReq": {
+                    "cType": "PK",
+                    "jnyCl": 2,
+                    "tvlrProf": [{"type": "E"}]
+                }
+            }
+        }],
+        "ver": "1.15"
+    }
+
+    # - Server fails in case of unicode escape sequences
+    #   hence, we have to handle the json serialization process
+    #   in order to set `ensure_ascii`
+    bestPriceSearchRequestStr = json.dumps(bestPriceSearchRequest, ensure_ascii=False, separators=(',', ':'))
+    bestPriceSearchRequestEncoded = bestPriceSearchRequestStr.encode('utf-8')
+
+    response = requests.post(url, params=params, headers=headers, data=bestPriceSearchRequestEncoded, proxies=proxies, verify=verify)
+    return response.json()
+
+
 def sampleStationSearch():
-    """Search for all train stations matching starting with 'Frank' """
+    """Sample request for all train stations starting with 'Frank' """
 
-    # - fix search term, until checksum is calculated dynamically
-    searchTerm = 'Frank'
+    print("sampleStationSearch")
 
-    # - query api
-    matchingStationsRes = searchStationByName(searchTerm)
+    # - request & response parsing
+    matchingStationsRes = searchStationByName()
+    matchingStations = [(station['name'], station['extId']) for station in matchingStationsRes['svcResL'][0]['res']['match']['locL']]
 
-    # - parse result
-    matchingStations = [(station['name'], station['extId']) for station in
-                        matchingStationsRes['svcResL'][0]['res']['match']['locL']]
     print(*matchingStations, sep='\n')
+    print("")
 
 
 def sampleConnectionSearch():
-    """Search for all connections from Frankfurt to Cologne on 20.05.2023"""
-    connections = searchConnection()
-    print(connections)
+    """Sample request for all connections from Frankfurt to Cologne on 20.05.2023"""
+
+    print("sampleConnectionSearch")
+
+    # - request & response parsing
+    connectionsResponse = searchConnection()
+    connections = [(connection['cid'], connection['dur']) for connection in connectionsResponse['svcResL'][0]['res']['outConL']]
+
+    print(*connections, sep='\n')
+    print("")
+
+
+def sampleResonstruction():
+    """
+    Sample request for available ticket options (e.g. 'Sparpreis') for the connection:
+        Frankfurt to Cologne from 25.05.2023 18:28-19:31 for ICE 10
+    """
+
+    print("sampleResonstruction")
+
+    connectionPrices = reconstruction()
+    ticket = [(ticket['fareL'][0]['name'], ticket['prc']) for ticket in connectionPrices['svcResL'][0]['res']['outConL'][0]['trfRes']['fareSetL']]
+
+    print(*ticket, sep='\n')
+    print("")
+
+
+def sampleBestPriceSearch():
+    """Sample request for best prices between Frankfurt -> Cologne on 25.05.2023 """
+
+    print("sampleBestPriceSearch")
+
+    # - request & response parsing
+    bestPriceResponse = bestPriceSearch()
+    bestPrices = [(bestPrice['toTime'], bestPrice['bestPrice']['amount']) for bestPrice in bestPriceResponse['svcResL'][0]['res']['outDaySegL']]
+
+    print(*bestPrices, sep='\n')
+    print("")
 
 
 def main():
-    """
-    Showcase for station and connection search
-    """
+    """ Demonstrate how to query various DB api endpoints """
+
     sampleStationSearch()
     sampleConnectionSearch()
+    sampleResonstruction()
+    sampleBestPriceSearch()
 
 
 if __name__ == '__main__':
